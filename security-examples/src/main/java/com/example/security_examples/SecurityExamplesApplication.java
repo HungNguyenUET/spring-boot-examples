6d9355a49aa628dde1d002325a45f944947b983d
hungnguyenuet <hungnguyenuet@gmail.com>
2024-12-29 08:55:56 +0700

    Init project

---
+++ security-examples/src/main/java/com/example/security_examples/SecurityExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExamplesApplication.class, args);
+	}
+
+}
